7fd87c5f5fc655254d8796bf78b2c6732adf5b1a
Osama Luhishi <oeluhishi@gmail.com>
2017-02-28 01:26:03 +0000

    Added 360 degree animation to splash screen logo - Need to delet Antirotat XML - Home.pc

--- Zonos-Net/app/src/main/java/com/zonosnetworks/zonos_net/Splash_Screen.java
+++ Zonos-Net/app/src/main/java/com/zonosnetworks/zonos_net/Splash_Screen.java
@@ -1,16 +1,47 @@
 package com.zonosnetworks.zonos_net;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-public class Splash_Screen extends AppCompatActivity {
+
+
+public class Splash_Screen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash__screen);
 
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        final Animation animation_1 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+        final Animation animation_2 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.abc_fade_out);
+
+        imageView.startAnimation(animation_1);
+        animation_1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.startAnimation(animation_2);
+                finish();
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
     }
+
+
 }
