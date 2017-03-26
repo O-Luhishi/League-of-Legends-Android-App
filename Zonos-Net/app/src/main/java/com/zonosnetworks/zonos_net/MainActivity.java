6bfa8b49eee40ed19d9079a5e6d3458b5d2300a4
Osama Luhishi Win PC <Oeluhishi@gmai.com>
2017-03-27 00:01:59 +0100

    Completed first page. learnt how to do slide. - Home.PC

--- Zonos-Net/app/src/main/java/com/zonosnetworks/zonos_net/MainActivity.java
+++ Zonos-Net/app/src/main/java/com/zonosnetworks/zonos_net/MainActivity.java
@@ -2,12 +2,26 @@ package com.zonosnetworks.zonos_net;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ViewFlipper;
 public class MainActivity extends AppCompatActivity {
-
+    Animation fade_in, fade_out;
+    ViewFlipper viewFlipper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewFlipper = (ViewFlipper) this.findViewById(R.id.bckgrndViewFlipper1);
+        fade_in = AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_in);
+        fade_out = AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_out);
+        viewFlipper.setInAnimation(fade_in);
+        viewFlipper.setOutAnimation(fade_out);
+//sets auto flipping
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.startFlipping();
     }
 }
