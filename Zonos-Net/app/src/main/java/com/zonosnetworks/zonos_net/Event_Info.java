024c91aa3fe3e5f3a358fe9811e96b8e01f4beae
Osama Luhishi Win PC <Oeluhishi@gmai.com>
2017-03-27 12:18:30 +0100

    New Page - Home.PC

---
+++ Zonos-Net/app/src/main/java/com/zonosnetworks/zonos_net/Event_Info.java
@@ -0,0 +1,13 @@
+package com.zonosnetworks.zonos_net;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Event_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event__info);
+    }
+}
